5dc6d9a770d7ed1aafdf8447f8f2c3ef424f76e6
Kwan-123 <75381919+Kwan-123@users.noreply.github.com>
2022-04-09 12:51:15 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 하명관 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/IphonMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IphonMapAppApp.swift
+//  Shared
+//
+//  Created by 하명관 on 2022/04/09.
+//
+
+import SwiftUI
+
+@main
+struct IphonMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
